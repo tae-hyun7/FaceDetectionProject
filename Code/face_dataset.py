cc4ba36c91f2462c8fcbf0bb5eb7f1b392b88aa1
Belien <56016354+SkullMurchen@users.noreply.github.com>
2020-12-15 14:31:57 +0900

    Update face_dataset.py

--- Code/face_dataset.py
+++ Code/face_dataset.py
@@ -7,11 +7,10 @@ cam.set(4, 480)
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 
-#for each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==> ')
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 
-#Initialize individual sampling face count
+
 count = 0
 while(True):
 	ret, img = cam.read()
@@ -21,7 +20,6 @@ while(True):
 	for (x,y,w,h) in faces:
 		cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
 		count += 1
-		# Save the captured image into the datasets folder
 		cv2.imwrite("dataset/user." + str(face_id) + '.' + str(count) + ".jpg",gray[y:y+h,x:x+w])
 		cv2.imshow('image', img)
 	k = cv2.waitKey(100) & 0xff #  press 'ESC' for exiting video
@@ -30,7 +28,6 @@ while(True):
 	elif count >= 30: # Take 30 face sample and stop video
 		break
 
-#Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
