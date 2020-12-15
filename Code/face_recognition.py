09ca4f5a1ec25b9d57e32122377904825331f891
taehyun7 <54888988+tae-hyun7@users.noreply.github.com>
2020-12-15 15:23:30 +0900

    Update face_recognition.py

--- Code/face_recognition.py
+++ Code/face_recognition.py
@@ -8,19 +8,14 @@ cascadePath = "haarcascades/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
 id = 0
-
-# names related to ids: example ==> loze: id=1,  etc
 # 사용자 이름 설정
 names = ['chan', 'LimTaeHyun', 'JangTaeYoung', 'Jongsub']
 
-# Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video widht
 cam.set(4, 480) # set video height
 
-# Define min window size to be recognized as a face
 minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 
@@ -39,7 +34,6 @@ while True:
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-        # Check if confidence is less them 100 ==> "0" is perfect match
         if (confidence < 100):
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
@@ -54,7 +48,6 @@ while True:
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
-# Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
